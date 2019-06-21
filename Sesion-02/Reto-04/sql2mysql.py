3bf2ad207e4afed9b482bf8581b453f360cbcf95
rctorr <rictor@cuhrt.com>
2019-06-21 14:27:20 -0500

    Se agrega descripción y archivos para Reto-04 en Sesion-02 en Student

---
+++ Sesion-02/Reto-04/sql2mysql.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Esta es la plantilla sobre la que tiene que iniciar para crear tu
+# código, a partir de aquí puedes comenzar.
+
+
+# Varias líneas después!
+
+
+# ¿En serio vas a escribir el script de nuevo?
+
+
+# Usa el script que has creado en el Ejemplo-04, reutiliza código,
+# copialo y remplaza este archivo.

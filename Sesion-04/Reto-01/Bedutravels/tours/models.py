b47abc51265c572732d4d2febb952feec8077b4e
Ricardo Torres <rictor@cuhrt.com>
2019-08-21 17:43:14 -0500

    Terminando cambio en Reto-01 en Sesion-03 a Bedutravels

--- Sesion-04/Reto-01/Bedutravels/tours/models.py
+++ Sesion-04/Reto-01/Bedutravels/tours/models.py
@@ -14,3 +14,19 @@ class User(models.Model):
     genero = models.CharField(max_length=1, choices=GENERO)
     clave = models.CharField(max_length=40, null=True, blank=True)
     tipo = models.CharField(max_length=45, null=True, blank=True)
+
+    def __str__(self):
+        """ Se define la representación en str para User """
+        return "{} {}".format(self.nombre, self.apellidos)
+
+
+class Zona(models.Model):
+    """ Define la tabla Zona """
+    nombre = models.CharField(max_length=45)
+    descripcion = models.CharField(max_length=256, null=True, blank=True)
+    latitud = models.DecimalField(max_digits=8, decimal_places=6, null=True, blank=True)
+    longitud = models.DecimalField(max_digits=8, decimal_places=6, null=True, blank=True)
+
+    def __str__(self):
+        """ Se define la representación en str para Zona """
+        return "{}".format(self.nombre)

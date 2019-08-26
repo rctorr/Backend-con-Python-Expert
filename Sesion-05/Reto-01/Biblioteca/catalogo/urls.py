9263a1034cc539de59779c90a1f69f5f85e7714e
Ricardo Torres <rictor@cuhrt.com>
2019-08-26 10:44:26 -0500

    Iniciando cambio a Bedutravels Reto-01, Sesion-05

--- Sesion-05/Reto-01/Biblioteca/catalogo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("prestamo/nuevo/", views.nuevo_prestamo, name="nuevo_prestamo"),
-    path("login/", views.login_user, name="login_user"),
-    path("logout/", views.logout_user, name="logout_user"),
-]
